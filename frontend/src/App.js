cc93f7ee948060a57fb4692204161f32e1bbd3af
Manuj Gogoi <manujgogoi@gmail.com>
2021-04-11 11:11:14 +0530

    react event testing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
 
 function App() {
+  const sayHello = () => {
+    console.log("Hello Console from react App");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <h4>Testing Github</h4>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <button onClick={sayHello}>Click</button>
     </div>
   );
 }
