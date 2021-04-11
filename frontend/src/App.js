650271728bd16bfed81ab5e6112a1b713c8746cc
Manuj Gogoi <manujgogoi@gmail.com>
2021-04-11 10:57:48 +0530

    Testing Github from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h3>This is custom Heading</h3>
+        <h4>Testing Github</h4>
         <a
           className="App-link"
           href="https://reactjs.org"
